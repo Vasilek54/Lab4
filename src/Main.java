2934b16c17d5dc7ec99c53535adb204da7a99a16
Vasilek54 <sonya_monya17@ukr.net>
2023-10-25 23:45:15 +0300

    Added user interface

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введіть слово або фразу для перевірки на паліндром. Введіть 'exit' для виходу.");
+
+        while (true) {
+            System.out.print("Введіть слово: ");
+            String input = scanner.nextLine();
+
+            if ("exit".equalsIgnoreCase(input)) {
+                System.out.println("Завершення програми. Дякуємо!");
+                break;
+            }
+
+            if (PalindromeChecker.isPalindrome(input)) {
+                System.out.println("'" + input + "' є паліндромом.");
+            } else {
+                System.out.println("'" + input + "' не є паліндромом.");
+            }
+        }
+
+        scanner.close();
     }
 }
